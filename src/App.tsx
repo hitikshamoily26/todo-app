29909109439118989cdd2fa44e9d3f6bbd962793
hitiksha26 <hitikshamoily26@gmail.com>
2025-11-16 20:18:51 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import ToDoItem from "./ToDoItem";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+
+type toDoProps = {
+  id: number
+  title: string
+  completed: boolean
+}
+
+export default function App() {
+  const [toDo, setToDo] = useState<toDoProps[]>([])
+  const [loading, setLoading] = useState(true)
+  const [newToDo, setNewToDo] = useState<string>('')
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => res.json())
+      .then((data: toDoProps[]) => {
+        setToDo(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Failed to fetch todo's", error)
+        setLoading(false);
+      })
+  }, [])
+
+  const toggleComplete = (id: number) => {
+    setToDo(prev =>
+      prev.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    )
+  }
+
+  const deleteToDo = (id: number) => {
+    setToDo(prev => prev.filter(todo => todo.id !== id));
+  };
+
+  const addToDo = () => {
+    if (newToDo.trim() === "") {
+      alert("Please enter a todo")
+      return;
+    }
+    const newToDOItem: toDoProps = {
+      id: toDo.length + 1,
+      title: newToDo,
+      completed: false
+    }
+    setToDo([...toDo, newToDOItem]);
+    setNewToDo("");
+  }
+
+  if (loading) {
+    return <h2 className="text-center mt-10">"Loading todos.."</h2>
+  }
+
+  return (<>
+    <h2 className="flex gap-2 mb-4">To do list: </h2>
+    <div className="flex flex-col mb-8">
+      <div className="w-1/2">
+        <Input
+          placeholder="Add a new todo"
+          value={newToDo}
+          onChange={(e) => setNewToDo(e.target.value)}
+          className="w-full"
+        />
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <Button onClick={addToDo}>
+          Add
+        </Button>
+      </div>
+    </div>
+
+    <ul className="space-y-2">
+      {toDo?.map((toDo, key) =>
+        (<ToDoItem key={key} toDoID={toDo.id} toDo={toDo.title} completed={toDo.completed} toggleComplete={toggleComplete} deleteToDo={deleteToDo}></ToDoItem>)
+      )}</ul>
+  </>)
+}
---
+++ src/ToDoItem.tsx
@@ -0,0 +1,31 @@
+type ToDoItemProp = {
+    toDoID: number
+    toDo: string
+    completed: boolean
+    toggleComplete: (id: number) => void;
+    deleteToDo: (id: number) => void;
+}
+
+export default function ToDoItem({ toDoID, toDo, completed, toggleComplete, deleteToDo }: ToDoItemProp) {
+    return (
+        <li key={toDoID} className="flex items-center gap-2">
+            <input
+                type="checkbox"
+                checked={completed}
+                onChange={() => toggleComplete(toDoID)}
+                className="h-4 w-4 accent-blue-500"
+            />
+
+            <span className={completed ? "line-through text-gray-400" : ""}>
+                {toDo}
+            </span>
+
+            <button
+                className="text-red-500 hover:text-red-700"
+                onClick={() => deleteToDo(toDoID)}
+            >
+                Delete
+            </button>
+        </li>
+    )
+}
